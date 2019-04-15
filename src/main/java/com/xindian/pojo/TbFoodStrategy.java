a0ec2c6411a836f474a3a80361dd10a96d553a84
Yangdarong <yxr800@icloud.com>
2019-04-15 18:37:18 +0800

    添加新的pojo

---
+++ src/main/java/com/xindian/pojo/TbFoodStrategy.java
@@ -0,0 +1,32 @@
+package com.xindian.pojo;
+
+public class TbFoodStrategy {
+
+    private int fsId;
+    private int fId;
+    private int sId;
+
+    public int getFsId() {
+        return fsId;
+    }
+
+    public void setFsId(int fsId) {
+        this.fsId = fsId;
+    }
+
+    public int getfId() {
+        return fId;
+    }
+
+    public void setfId(int fId) {
+        this.fId = fId;
+    }
+
+    public int getsId() {
+        return sId;
+    }
+
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+}
---
+++ src/main/java/com/xindian/pojo/TbStrategy.java
@@ -0,0 +1,42 @@
+package com.xindian.pojo;
+
+public class TbStrategy {
+
+    private int sId;
+    private String sName;
+    private String sContext;
+
+    private int uId;
+
+    public int getsId() {
+        return sId;
+    }
+
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsContext() {
+        return sContext;
+    }
+
+    public void setsContext(String sContext) {
+        this.sContext = sContext;
+    }
+
+    public int getuId() {
+        return uId;
+    }
+
+    public void setuId(int uId) {
+        this.uId = uId;
+    }
+}
